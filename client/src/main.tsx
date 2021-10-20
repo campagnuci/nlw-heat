367a1bd4671d1ea92a676bbbe6a93805a866687e
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2021-10-20 14:14:00 -0300

    content from 2nd day - frontend

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { App } from './App'
+import { AuthProvider } from './context/auth'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
